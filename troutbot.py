8fd31f637a6f2d82e13920fc2b110a0aeb765be0
JJPMaster <78509994+JJPMaster@users.noreply.github.com>
2021-02-19 21:48:55 -0500

    Update troutbot.py

--- troutbot.py
+++ troutbot.py
@@ -7,7 +7,8 @@ import aiohttp
 import os
 TOKEN = os.environ['DISCORD_TOKEN']
 image = 'px-Rainbow_trout_transparent.png'
-self = '812453966025719808'
+self = 812453966025719808
+
 @bot.event
 async def on_ready():
 	print(f'Bot connected as {bot.user}')
@@ -16,10 +17,8 @@ async def on_ready():
 @bot.event
 async def on_message(message):
 	if message.content.startswith('/trout'):
-		if message.author.id != self:
 			await message.channel.send('{} slaps {} around a bit with a large trout'.format(message.author.mention, message.content[7:]), file=discord.File(image))
-		else:
-			await message.channel.send('Please refrain from doing that in the future.')
+
 @bot.command()
 async def trout(ctx):
 	await ctx.channel.send('hi')
